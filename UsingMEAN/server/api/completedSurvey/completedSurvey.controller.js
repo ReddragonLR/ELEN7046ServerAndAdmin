e672e25b6b2759a23991ce623db91d0e95df763b
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2015-06-23 20:09:07 +0200

    Added icons and completedSurveyDateCompleted value being set to Date.UTC() on create

--- UsingMEAN/server/api/completedSurvey/completedSurvey.controller.js
+++ UsingMEAN/server/api/completedSurvey/completedSurvey.controller.js
@@ -50,6 +50,7 @@ exports.create = function (req, res) {
         console.log(req.body);
         var completedSurveyCollection = req.body;
         for (var i = 0; i < completedSurveyCollection.length; i++) {
+            completedSurveyCollection[i].DateCompleted = Date.UTC();
             CompletedSurvey.create(completedSurveyCollection[i], function (err, completedSurvey) {
                 if (err) {
                     return handleError(res, err);
